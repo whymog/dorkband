1fef96e1752e2054566a87f81cb085bba4e0ac5a
Nick Cummings <nickmcummings@gmail.com>
2019-09-13 23:50:10 -0700

    Fix for GH Pages deployment.

---
+++ precache-manifest.003d94ec36275afbd709be0dfc092a35.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53e3e028aee923aecb248dc963873f58",
+    "url": "/dorkband/index.html"
+  },
+  {
+    "revision": "9781870870e0cca7d4af",
+    "url": "/dorkband/static/css/main.0df31758.chunk.css"
+  },
+  {
+    "revision": "9b69c29456ad6f735d7a",
+    "url": "/dorkband/static/js/2.1644df9d.chunk.js"
+  },
+  {
+    "revision": "9781870870e0cca7d4af",
+    "url": "/dorkband/static/js/main.81eea29f.chunk.js"
+  },
+  {
+    "revision": "bdccda4322913e9faf16",
+    "url": "/dorkband/static/js/runtime~main.821df4cf.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.788ea08cfd45aa493b656f4f16b9f555.js"
+  "/dorkband/precache-manifest.003d94ec36275afbd709be0dfc092a35.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/dorkband/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
